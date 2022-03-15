deb3f9b4c3774fc371523d03954a0949a63ab3f0
Billy⚡️ <will.js@protonmail.com>
2022-03-15 09:09:05 -0500

    Much better

--- questHelper.user.js
+++ questHelper.user.js
@@ -1,163 +1,214 @@
 // ==UserScript==
-// @name        Quest Helper
+// @name        Test
 // @namespace   Marascripts
-// @match       https://www.marapets.com/shops.php*
-// @match       https://www.marapets.com/shop.php*
-// @match       https://www.marapets.com/farm.php
-// @match       https://www.marapets.com/leprechaun.php
 // @match       https://www.marapets.com/carpenter.php
-// @match       https://www.marapets.com/explorer.php
+// @match       https://www.marapets.com/shop.php
+// @match       https://www.marapets.com/shops.php
 // @match       https://www.marapets.com/attic.php
-// @match       https://www.marapets.com/inn.php
-// @match       https://www.marapets.com/truck.php
-// @match       https://www.marapets.com/bee.php
-// @match       https://www.marapets.com/excavator.php
 // @match       https://www.marapets.com/elger.php
+// @match       https://www.marapets.com/bee.php
 // @match       https://www.marapets.com/brew.php
-// @match       https://www.marapets.com/traveller.php
-// @match       https://www.marapets.com/inn.php
-// @match       https://www.marapets.com/monster.php
-// @match       https://www.marapets.com/computer.php
-// @match       https://www.marapets.com/truck.php
-// @match       https://www.marapets.com/santa.php
-// @match       https://www.marapets.com/garage.php
-// @match       https://www.marapets.com/stalker.php
 // @match       https://www.marapets.com/viewstock.php
-// @grant       GM_setValue
-// @grant       GM_getValue
 // @version     1.0
 // @author      themagicteeth
-// @description Automates buying quest items.
-// @license     MIT
+// @description Quest automater for quests.
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @run-at      document-idle
 // ==/UserScript==
 
-/**
- * KNOWN BUGS:
- * - Does not take from users own shop
- * - If two items are the same it will loop forever
- **/
 
-const questNpc = document.querySelector(".mainfeature_art")
-const quest = GM_getValue("quest")
+/****************************************************************
+ * 
+ * HELPER FUNCTIONS
+ * 
+ ****************************************************************/
 
-if (questNpc?.innerText.includes("Complete more Quests")) {
-  GM_setValue("quest", document.URL) // Set the quest URL
+/* Compares user price to shop price */
+function isUserShopCheaper() {
+    const userPrice = parseInt(document.querySelector(".alsotry.same.strong").innerText.split(" ")[2].split("MP")[0].replace(/,/g, ""))
+    const shopPrice = parseInt(document.querySelector("span.sitedate.same.italic").innerText.split(" ").pop().split("MP")[0].replace(/,/g, ""))
+    return shopPrice <= userPrice ? false : true
+}
 
-  const acceptQuest = document.querySelector("input[value='Accept Quest']")
-  if (acceptQuest) { acceptQuest.click() }
+/* Checks if an item is in stock */
+function checkInStock() {
+    const itemSource = document.querySelector(".pricechecktable .sitedate.same.italic")
+    if (itemSource) {
+        const stock = itemSource.innerText
+        return (stock.includes("stock") && stock.split(" ")[0] !== "0") ? itemSource.parentElement.href : null
+    } else { return null }
+}
 
-  const questAgain = document.querySelector("form[action='?do=quest'] input")
-  if (questAgain) { questAgain.click() }
+/* Checks if an item is retired */
+function checkRetired() {
+    const retired = document.querySelector(".pricechecktable .banned.same.italic")
+    const retiredAlt = document.querySelector(".pricechecktable .offline.same.italic")
+    return retired || retiredAlt ? true : false
+}
 
-  const computer = document.querySelector("input[value='Quest Computer Repair Again']")
-  if (computer) { computer.click() }
-  
-  const puchalla = document.querySelector("input[value='Quest Puchalla Inn Again']")
-  if (puchalla) { puchalla.click() }
-  
-  else {
-    const checkPrice = document.querySelectorAll(".pricecheck")
-    const numItems = checkPrice.length
-    let acquiredCount = 0
+/* Checks if item is in the attic */
+function getAttic() {
+    const atticCount = document.querySelector(".pricecheckcontent .banned.same")
+    return parseInt(atticCount.innerText.split(" ")[0]) === 0 ? null : atticCount.parentElement.href
+}
 
-    for (const item of checkPrice) {
-      // This is the checkmark that is next to items acquired
-      const acquired = item.parentElement.parentElement.previousElementSibling.querySelector("img[src='https://images.marapets.com/tick.png']")
+/* Gets the direct URL to buy from user shop */
+function getUserShop() {
+    const userShopLink = document.querySelector(".pricechecktable .alsotry.same.strong")
+    return userShopLink ? userShopLink.parentElement.href : null
+}
 
-      // If we have acquired the item increase the count of acquired items
-      if (acquired) {
-        // Increment the acquired items
-        acquiredCount += 1
+/* Navigates to a given URL */
+function goToItem(url) {
+    window.location.href = url
+}
 
-        // Complete quest button
-        const completeQuest = document.querySelector("input[value='Complete Quest']")
+/* Checks if a captcha is present */
+function checkCaptcha() {
+    return document.getElementById("securitycode") ? true : false
+}
 
-        // If we have acquired the items and there is no captcha, complete the quest
-        if (acquiredCount === numItems && !document.getElementById("securitycode")) {
-          completeQuest.click()
+function setQuestUrl() {
+    const questGiver = document.querySelector(".maralayoutmiddle .mainfeature_start .mainfeature_npc")
+    if (questGiver) {
+        const moreQuests = questGiver.innerText.trim()
+        if (moreQuests === "Complete more Quests") {
+            const questUrl = document.URL.split("?")[0]
+            GM_setValue("quest", questUrl)
         }
-
-        // There is a captcha, focus it and submit when 6 characters are entered
-        else if (document.getElementById("securitycode")) {
-          const captcha = document.querySelector("input[name='code']") // Captcha input field
-          captcha.oninput = () => {
-            if (captcha.value.length === 6) {
-              completeQuest.click()
-            }
-          }
-          captcha.focus()
-        }
-      }
-
-      // If we have not acquired it, add event listener to the "Price Check" link
-      else {
-        item.addEventListener('click', (e) => {
-          setTimeout(() => {
-            const priceCheck = document.querySelector(".pricechecktable")
-
-            // We need to have the item source in order to buy it (duh)
-            if (priceCheck) {
-              const userShopLink = document.querySelector(".pricechecktable .alsotry.same.strong")
-
-              // If we have any in our attic, use those
-              const atticCount = parseInt(document.querySelector(".pricecheckcontent .banned.same").innerText.split(" ")[0])
-              if (atticCount !== 0) { window.location.href = document.querySelector(".pricecheckcontent .banned.same").parentElement.href }
-              
-              else {
-                // The item is retired, so buy from a user shop
-                const retired = document.querySelector(".pricechecktable .banned.same.italic")
-                const retiredAlt = document.querySelector(".pricechecktable .offline.same.italic")
-                if (retired || retiredAlt) { window.location.href = userShopLink.parentElement.href }
-
-                // The item is sold at a shop
-                const itemSource = document.querySelector(".pricechecktable .sitedate.same.italic")
-                if (itemSource) {
-                  const stock = itemSource.innerText
-                  // The item must have a stock quantity more than 0
-                  if (stock.includes("stock") && stock.split(" ")[0] !== "0") {
-                    const userPrice = parseInt(document.querySelector(".alsotry.same.strong").innerText.split(" ")[2].split("MP")[0].replace(/,/g, ""))
-                    const shopPrice = parseInt(document.querySelector("span.sitedate.same.italic").innerText.split(" ").pop().split("MP")[0].replace(/,/g, ""))
-
-                    // Compare the shops price, to the users price and buy the cheaper
-                    if (shopPrice <= userPrice) { window.location.href = itemSource.parentElement.href }
-                    else { window.location.href = userShopLink.parentElement.href }
-                  }
-
-                  // Item is not in stock
-                  else { window.location.href = userShopLink.parentElement.href }
-                }
-              }
-            }
-          }, 500)
-        })
-      }
     }
-  }
 }
 
-if (document.URL.includes("/attic.php")) {
-  if (!document.URL.includes("?remove=1")) {
-    const inventory = document.querySelector("input[value='Inventory']")
-    if (inventory) { inventory.click() }
-  }
-  // We removed from our attic, back to the quest
-  else { window.location.href = quest }
+function getItems() {
+    const questItemsTable = document.querySelector(".bigsearchbox.middleit .flex-table2")
+
+    if (questItemsTable) {
+        const itemsNeeded = {}
+        const itemElements = [...questItemsTable.children]
+
+        let index = 0
+        itemElements.forEach((item) => {
+            const obtained = item.querySelector(".bigger").innerHTML.includes("/tick.png")
+
+            if (!obtained) {
+                const itemName = item.querySelector(".bigger").innerText
+                const priceCheck = item.querySelector(".petpadding a").getAttribute("data-id")
+
+                itemsNeeded[index] = {
+                    "name": itemName,
+                    "check": priceCheck
+                }
+
+                index += 1
+            }
+        })
+      
+        GM_setValue("items", itemsNeeded)
+        return itemsNeeded
+    }
 }
 
-// Bought from user shop, or was in our shop
-if (document.URL.includes("/shops.php")|| document.URL.includes("/viewstock.php")) { window.location.href = quest }
+async function checkFirstItem() {
+    const questItems = getItems()
+    
+    // Check if we still need items
+    if (questItems[0]) {
+        // Click "Check Price" on the item based on its "data-id" field
+        const itemId = questItems[0]["check"] // Items "data-id" field
+        document.querySelector(`a[data-id='${itemId}']`).click()
 
-// Item is in stock
-if (document.URL.includes("/shop.php")) {
-  const itemToBuy = document.querySelector("div.marapets_border5 a") // Item with a dotted blue border
-  if (itemToBuy) {
-    itemToBuy.click()
-  } else {
-    window.location.href = quest
-  }
+        setTimeout(function () {
+            let itemURL = "" // Stores the URL for the best place to buy the item
 
-  const buyItem = document.querySelector("button.g-recaptcha")
-  if (buyItem) {
-    buyItem.click()
-  }
+            // First check if we have the item in our attic
+            // TODO: Check price due to bug of magic beans
+            const atticURL = getAttic()
+            if (atticURL !== null) { itemURL = atticURL }
+
+            // If it is not in our attic perform other checks
+            else {
+                const userShopURL = getUserShop() // Direct URL to buy from user shop
+                const inStockURL = checkInStock() // URL to the shop where item is sold
+
+                // Check if the item is retired, if so, buy from user shop
+                const retired = checkRetired()
+                if (retired || !inStockURL) { itemURL = userShopURL }
+
+                // Item is not retired, and is in stock
+                else {
+                    const userCheaper = isUserShopCheaper() // Compare user shop price to NPC shop
+                    if (!userCheaper) { itemURL = inStockURL } // NPC shop is cheaper
+                    else { itemURL = userShopURL } // User is cheaper
+                }
+            }
+
+            goToItem(itemURL)
+        }, 1500)
+    } else if (!checkCaptcha()) { document.querySelector("input[value='Complete Quest']").click() }
 }
+
+function getLocation() {
+    // Set the URL of the current quest
+    setQuestUrl()
+    const questURL = GM_getValue("quest", "")
+
+    // Check if the current page is the questgiver
+    if (document.URL.includes(questURL)) {
+        // Check if we have completed a quest and need to start a new one
+        const message = document.querySelector(".maralayoutmiddle div.bigger.middleit.btmpad6")
+        if (message) {
+            if (message.innerText.includes("Thank you for finishing my quest")) {
+                const questAgain = document.querySelector(".maralayoutmiddle form input")
+                questAgain.click()
+            }
+        }
+      
+        // If Queen Bee stings a pet
+        //const queenBeeSting = document.querySelector(".maralayoutmiddle a .bigger.petpadding")
+        // else if (queenBeeSting) {
+        //   if (queenBeeSting.includes("Queen Bee stung your pet!")) {
+        //     document.querySelector("input[value='Quest Queen Bee Again']").click()
+        //   }
+        // }
+      
+        // If we don't need to start a new one, check the items
+        else { checkFirstItem() }
+    }
+
+    // We bought the item from a user shop, go back to the questgiver
+    if (document.URL.includes("/shops.php")) { window.location = questURL }
+
+    // We are in an NPC shop
+    if (document.URL.includes("/shop.php")) {
+        // Find the item (dotted blue border), and click it
+        const itemToBuy = document.querySelector("div.marapets_border5 a") // Item with a dotted blue border
+        if (itemToBuy) { itemToBuy.click() }
+
+        // If we have just bought the item, go back to the quest
+        const thanksForBuying = document.querySelector(".bigger.middleit.btmpad6")
+        if (thanksForBuying) { window.location = questURL }
+    }
+    
+    // We are buying an item from an NPC shop
+    if (document.URL.includes("/shop.php?do=buy&id=")) {
+        const buyButton = document.querySelector("button")
+        buyButton.click()
+    }
+
+    // We had the item in out attic
+    if (document.URL.includes("/attic.php")) {
+        // If we have not yet removed the item, remove it
+        if (!document.URL.includes("?remove=1")) {
+            const inventory = document.querySelector("input[value='Inventory']")
+            if (inventory) { inventory.click() }
+        }
+        // We removed from our attic, back to the quest
+        else { window.location.href = questURL }
+    }
+
+    // Our shop was lowest price, go back to quest giver
+    if (document.URL.includes("/viewstock.php")) { window.location.href = questURL }
+}
+
+getLocation()
