ad284be93fef24e23b16343e07f8bacb5adba756
Billy <will.js@outlook.com>
2022-04-01 16:43:13 -0500

    Sultan not working atm

--- games/simpleGames.user.js
+++ games/simpleGames.user.js
@@ -31,30 +31,32 @@
 
 
 function clickButton(buttonValue) {
-    const button = document.querySelector(".maralayoutmiddle input[type='submit']").value
-    if (button.search(buttonValue > 0)) {
-        button.click()
+    const button = document.querySelector(`input[value='${buttonValue}']`)
+    if (button) {
+      button.click()
     }
-}
-
-if (!document.querySelector("div.middleit.comebackbox")) {
-    if (document.URL.includes("sugarstack")) { clickButton('Play for 1,000MP') }
-    if (document.URL.includes("telescope")) { clickButton('Use for 2,000MP') }
-    if (document.URL.includes("gumball")) { clickButton('Put in 150MP') }
-    if (document.URL.includes("trash")) { clickButton('Grab Trash') }
-    if (document.URL.includes("undyingfairy")) { clickButton('Spin Wheel') }
-    if (document.URL.includes("sewage")) { clickButton('Swim in the Sewage') }
-    if (document.URL.includes("potofgold")) { clickButton('Grab Prize') }
-    if (document.URL.includes("giganticfairy")) { clickButton('Test Your Strength') }
-    if (document.URL.includes("plushies")) { clickButton('Move Down') }
-    if (document.URL.includes("sevenheaven")) { clickButton('Roll Again') }
-    if (document.URL.includes("rack")) { clickButton('Take Free Clothing') }
-    if (document.URL.includes("graverobbing")) { clickButton('Rob a Grave') }
-    if (document.URL.includes("archeology")) { clickButton("Dig for Simerian Artifacts") }
-    if (document.URL.includes("ants.php")) { clickButton("Grab Prize") }
-    if (document.URL.includes("vending.php")) { clickButton("Use Vending Machine") }
-    if (document.URL.includes("fishing.php")) { clickButton("Go Fishing") }
-    if (document.URL.includes("/tree.php")) { clickButton("Shake Tree") } // Shake for the Avatar
-    if (document.URL.includes("scratchcards2.php")) { clickButton("Buy Scratchcard") }
-    if (document.URL.includes("sultan")) { clickButton("Tax") }
-}
+  }
+  
+  
+  if (!document.querySelector("div.middleit.comebackbox")) {
+      if (document.URL.includes("sugarstack")) { clickButton('Play for 1,000MP') }
+      if (document.URL.includes("telescope")) { clickButton('Use for 2,000MP') }
+      if (document.URL.includes("gumball")) { clickButton('Put in 150MP') }
+      if (document.URL.includes("trash")) { clickButton('Grab Trash') }
+      if (document.URL.includes("undyingfairy")) { clickButton('Spin Wheel') }
+      if (document.URL.includes("sewage")) { clickButton('Swim in the Sewage') }
+      if (document.URL.includes("potofgold")) { clickButton('Grab Prize') }
+      if (document.URL.includes("giganticfairy")) { clickButton('Test Your Strength') }
+      if (document.URL.includes("plushies")) { clickButton('Move Down') }
+      if (document.URL.includes("sevenheaven")) { clickButton('Roll Again') }
+      if (document.URL.includes("rack")) { clickButton('Take Free Clothing') }
+      if (document.URL.includes("graverobbing")) { clickButton('Rob a Grave') }
+      if (document.URL.includes("archeology")) { clickButton("Dig for Simerian Artifacts") }
+      if (document.URL.includes("ants.php")) { clickButton("Grab Prize") }
+      if (document.URL.includes("vending.php")) { clickButton("Use Vending Machine") }
+      if (document.URL.includes("fishing.php")) { clickButton("Go Fishing") }
+      if (document.URL.includes("/tree.php")) { clickButton("Shake Tree") } // Shake for the Avatar
+      if (document.URL.includes("scratchcards2.php")) { clickButton("Buy Scratchcard") }
+      //if (document.URL.includes("sultan")) { clickButton("Tax") }
+  }
+  
