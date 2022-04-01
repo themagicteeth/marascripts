563b1971d22a4e605652a2aebd5f8e55d66d9a28
Billy <will.js@outlook.com>
2022-04-01 11:38:28 -0500

    Add BP automater

---
+++ dailies/bpAuto.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name        BP Dailies Automator
+// @namespace   Marascripts
+// @match       https://www.marapets.com/newsagent.php
+// @match       https://www.marapets.com/sword.php
+// @match       https://www.marapets.com/magazines.php
+// @version     1.0.0
+// @author      themagicteeth
+// @description Automates BP dailies
+// @license     MIT
+// ==/UserScript==
+
+if (!document.querySelector("div.middleit.comebackbox")) {
+
+    function clickButton(buttonValue) {
+        const button = document.querySelector(`input[value='${buttonValue}']`)
+        if (button) {
+            button.click()
+        }
+    }
+
+    if (document.URL.includes("/sword.php")) { clickButton("Lift Sword from the Stone") }
+    if (document.URL.includes("/magazines.php")) { clickButton("Buy Magazine") }
+    if (document.URL.includes("/newsagent.php")) { clickButton("Buy Newspaper") }
+}
