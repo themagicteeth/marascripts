927e2b127550b45cb00a7a3198c9518e45c98b08
Billy⚡️ <will.js@protonmail.com>
2022-04-18 09:13:25 -0500

    Add timer

--- olympics/olympicsHelper.user.js
+++ olympics/olympicsHelper.user.js
@@ -6,46 +6,48 @@
 // @version     1.0
 // @author      themagicteeth
 // @license     MIT
-// @description Picks the ideal event for each pet.
+// @description Picks the optimal Olympic event for each of your pets.
 // ==/UserScript==
 
-/* *
- * This picks the ideal event for each of your pets.
- * 
- * TODO:
- *  - Base chosen event on rewards
- *  - Add "Olympian" condition
- * */
+const events = document.querySelectorAll(".eachpet_box.marapets_border")
 
-setTimeout(() => {
-  const events = document.querySelectorAll(".eachpet_box.marapets_border")
-
-  if (events.length > 0) {
+if (events.length > 0) {
+    const olympian = []
     const ultimate = []
     const expert = []
     const intermediate = []
     const beginner = []
     const untrained = []
 
-    for (const eventElement of events) {
-      const level = eventElement.querySelector(".alsotry").innerText
-      const enterUrl = eventElement.querySelector("a").href
+    setTimeout(() => {
+        for (let event = 0; event < events.length; event++) {
+            const level = events[event].querySelector(".alsotry").innerText
+            const enterUrl = events[event].querySelector("a").href
 
-      if (level === "Ultimate") { ultimate.push(enterUrl) }
-      else if (level === "Expert") { expert.push(enterUrl) }
-      else if (level === "Intermediate") { intermediate.push(enterUrl) }
-      else if  (level === "Beginner") { beginner.push(enterUrl) }
-      else if  (level === "Untrained")  { untrained.push(enterUrl) }
+            if (level.split(" ")[0] === "Olympian") { olympian.push(enterUrl) }
+            else if (level === "Ultimate") { ultimate.push(enterUrl) }
+            else if (level === "Expert") { expert.push(enterUrl) }
+            else if (level === "Intermediate") { intermediate.push(enterUrl) }
+            else if (level === "Beginner") { beginner.push(enterUrl) }
+            else if (level === "Untrained") { untrained.push(enterUrl) }
+        }
+
+        if (ultimate.length > 0) { window.location.href = ultimate[0] }
+        else if (expert.length > 0) { window.location.href = expert[0] }
+        else if (intermediate.length > 0) { window.location.href = intermediate[0] }
+        else if (beginner.length > 0) { window.location.href = beginner[0] }
+        else { window.location.href = untrained[0] }
+    }, 1200)
+}
+else if (document.querySelector(".bigger.petpadding")) {
+    const entered = document.querySelector(".bigger.petpadding")
+    if (entered.innerText.search("entered") > 0) {
+        window.location.href = "https://www.marapets.com/competitions.php"
     }
-
-    if (ultimate.length > 0) { window.location.href = ultimate[0]}
-    else if (expert.length > 0) { window.location.href = expert[0] }
-    else if (intermediate.length > 0) { window.location.href = intermediate[0] }
-    else if (beginner.length > 0) { window.location.href = beginner[0] }
-    else { window.location.href = untrained[0] }
-  }
-
-  else if (!document.querySelector("div.middleit.comebackbox")) {
-    window.location.href = "https://www.marapets.com/competitions.php" 
-  }
-}, 1200);
+}
+else {
+    setTimeout(() => {
+        timeout = 5000
+        window.location.href = "https://www.marapets.com/competitions.php"
+    }, 100000);
+}
