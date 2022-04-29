395915878ed17eb0ae9dd238abbdb5ad657fb19f
Billy⚡️ <will.js@protonmail.com>
2022-04-28 21:21:20 -0500

    Update shopPricerAuto.user.js

--- shop/shopPricerAuto.user.js
+++ shop/shopPricerAuto.user.js
@@ -10,51 +10,57 @@
 // @description Automates repricing shop, requires the Gift Box
 // ==/UserScript==
 
-// Set onclick to shopkeeper image to start the autpricer
-let autoPricerOn = GM_getValue('autoprice', 0) // Defaults to 0
-document.querySelector(".mainfeature_art").onclick = GM_setValue('autoprice', 1) // Set to 1 when we click the shopkeeper
+if (document.referrer.includes("/viewstock")) {
+    /** 
+     * Set this to the amount of seconds to delay, in milliseconds 
+     * */ 
+    const TIMEOUT = 40000
 
-// If the autoprice value is set to 1 (after clicking shopkeeper)
-if (autoPricerOn === 1) {
-    const noReprice = document.querySelector(".maralayoutmiddle .bigger.banned.middleit") // Nothing to reprice
-    const changed = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search('You have changed') // Confirmed reprices
-    const autoPriced = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search('has changed') // Repricer changed prices
+    // Set onclick to shopkeeper image to start the autpricer
+    const autoPricerOn = GM_getValue('autoprice', 0) // Defaults to 0
+    document.querySelector(".mainfeature_art").onclick = GM_setValue('autoprice', 1) // Set to 1 when we click the shopkeeper
 
-    // Click the "Auto Price" button, if none of the messages are present
-    if (autoPriced === -1 && changed === -1 && !noReprice) {
-        document.querySelector("input[value='Auto Price']").click()
-    }
+    // If the autoprice value is set to 1 (after clicking shopkeeper)
+    if (autoPricerOn === 1) {
+        const noReprice = document.querySelector(".maralayoutmiddle .bigger.banned.middleit") // Nothing to reprice
+        const changed = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search('You have changed') // Confirmed reprices
+        const autoPriced = document.querySelector(".maralayoutmiddle .bigger.middleit").innerText.search('has changed') // Repricer changed prices
 
-    // Click the "Update Prices" button, if the auto pricer message is there
-    if (autoPriced !== -1) {
-        const largeDrop = document.querySelectorAll(".marapets_border15")
-        // const mediumDrop = document.querySelectorAll(".marapets_border14")
-        if (largeDrop.length !== 0) {
-            largeDrop.forEach((item) => {
-                const oldPrice = item.querySelector(".mp").innerText.split("MP")[0].replace(/,/g, "")
-                item.querySelector("input").value = oldPrice
-            })
+        // Click the "Auto Price" button, if none of the messages are present
+        if (autoPriced === -1 && changed === -1 && !noReprice) {
+            document.querySelector("input[value='Auto Price']").click()
         }
 
-        document.querySelector("input[value='Update Prices']").click()
-    }
+        // Click the "Update Prices" button, if the auto pricer message is there
+        if (autoPriced !== -1) {
+            const largeDrop = document.querySelectorAll(".marapets_border15")
+            // const mediumDrop = document.querySelectorAll(".marapets_border14")
+            if (largeDrop.length !== 0) {
+                largeDrop.forEach((item) => {
+                    const oldPrice = item.querySelector(".mp").innerText.split("MP")[0].replace(/,/g, "")
+                    item.querySelector("input").value = oldPrice
+                })
+            }
 
-    // Go to the next page, or alert that we are finished
-    if (noReprice || changed !== -1) {
-        const pageButtons = document.querySelectorAll('.pages_all')[0].querySelectorAll('.pages_each a') // Array of the page selector
-        const secondToLastPageButton = pageButtons[pageButtons.length - 2] // The second to last button
+            document.querySelector("input[value='Update Prices']").click()
+        }
 
-        // If the second to last button says "Next >", click it otherwise notify we are finished
-        if (secondToLastPageButton.innerText === 'Next ›') {
-            secondToLastPageButton.click()
-        } else {
-            GM_setValue('autoprice', 0) // We are finished, set back to 0
-            //alert('Pricing finished!')
-          setTimeout(()=>{
-            GM_setValue('autoprice', 1)
-            window.location.href = "https://www.marapets.com/viewstock.php?page=1"
-           // Set this to the amount of miliseconds, default 20 seconds
-          },20000)
+        // Go to the next page, or alert that we are finished
+        if (noReprice || changed !== -1) {
+            const pageButtons = document.querySelectorAll('.pages_all')[0].querySelectorAll('.pages_each a') // Array of the page selector
+            const secondToLastPageButton = pageButtons[pageButtons.length - 2] // The second to last button
+
+            // If the second to last button says "Next >", click it otherwise notify we are finished
+            if (secondToLastPageButton.innerText === 'Next ›') {
+                secondToLastPageButton.click()
+            } else {
+                GM_setValue('autoprice', 0) // We are finished, set back to 0
+                //alert('Pricing finished!')
+                setTimeout(() => {
+                    GM_setValue('autoprice', 1)
+                    window.location.href = "https://www.marapets.com/viewstock.php?page=1"
+                }, TIMEOUT)
+            }
         }
     }
 }
