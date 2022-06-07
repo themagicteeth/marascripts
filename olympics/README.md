67345e94eb0563d8766513f5b88e63d075a21454
Will Schulzetenberg <wschulzetenberg@wddco.com>
2022-06-07 13:30:25 -0500

    Cleaner if loops

--- olympics/README.md
+++
@@ -1,9 +0,0 @@
-# Marascripts - Olympics
-
-## Olympics Helper - [Download](https://github.com/themagicteeth/marascripts/raw/main/olympics/olympicsHelper.user.js)
-Picks the ideal event for all pets.
-* Currently does NOT choose ANY Olympian level events
-
-
-## Olympics Rewards - [Download](https://github.com/themagicteeth/marascripts/raw/main/olympics/olympicsRewards.user.js)
-Colors the events based on the reward if you win.
--- scripts/olympicsHelper.user.js
+++ scripts/olympicsHelper.user.js
@@ -23,34 +23,20 @@ if (events.length > 0) {
       const enterUrl = eventElement.querySelector("a")
       enterUrl.onclick = null
 
-      if (level.split(" ")[0] === "Olympian") {
-        olympian.push(enterUrl)
-      } else if (level === "Ultimate") {
-        ultimate.push(enterUrl)
-      } else if (level === "Expert") {
-        expert.push(enterUrl)
-      } else if (level === "Intermediate") {
-        intermediate.push(enterUrl)
-      } else if (level === "Beginner") {
-        beginner.push(enterUrl)
-      } else if (level === "Untrained") {
-        untrained.push(enterUrl)
-      }
+      if (level.split(" ")[0] === "Olympian") olympian.push(enterUrl)
+      else if (level === "Ultimate") ultimate.push(enterUrl)
+      else if (level === "Expert") expert.push(enterUrl)
+      else if (level === "Intermediate") intermediate.push(enterUrl)
+      else if (level === "Beginner") beginner.push(enterUrl)
+      else if (level === "Untrained") untrained.push(enterUrl)
     }
 
-    if (olympian.length > 0) {
-      olympian[0].click()
-    } else if (ultimate.length > 0) {
-      ultimate[0].click()
-    } else if (expert.length > 0) {
-      expert[0].click()
-    } else if (intermediate.length > 0) {
-      intermediate[0].click()
-    } else if (beginner.length > 0) {
-      beginner[0].click()
-    } else {
-      window.location.href = untrained[0].click()
-    }
+    if (olympian.length > 0) olympian[0].click()
+    else if (ultimate.length > 0) ultimate[0].click()
+    else if (expert.length > 0) expert[0].click()
+    else if (intermediate.length > 0) intermediate[0].click()
+    else if (beginner.length > 0) beginner[0].click()
+    else untrained[0].click()
   }, 1200)
 } else if (document.querySelector(".bigger.petpadding")) {
   const entered = document.querySelector(".bigger.petpadding")
